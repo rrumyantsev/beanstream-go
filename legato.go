548a142f1ae66883f29588e55183916971ae7524
bowens <bowens@beanstream.com>
2015-04-02 12:11:26 -0700

    legato

--- legato.go
+++ legato.go
@@ -1,7 +1,7 @@
 package beanstream
 
 import (
-	httpMethods "github.com/Beanstream-DRWP/beanstream-go/httpMethods"
+	"github.com/Beanstream-DRWP/beanstream-go/httpMethods"
 )
 
 const url = "https://www.beanstream.com/scripts/tokenization/tokens"
